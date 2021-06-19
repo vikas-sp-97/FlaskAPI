918c9e5e416d713785907959143ed98217e10869
vikassp <vikas.p@evivehealth.com>
2021-06-19 22:13:08 +0530

    adding JWT auth module with sqlite3 DB accessors.

--- bookItemsApi/security.py
+++ bookItemsApi/security.py
@@ -2,23 +2,16 @@ from werkzeug.security import safe_str_cmp
 import yaml
 from FlaskAPI.bookItemsApi.user import User
 
-users = [
-    User(1, 'vikas', 'test')
-]
-
-user_mapping = {u.username: u for u in users}
-user_id_mapping = {u.id: u for u in users}
-
 
 def authenticate(username, password):
-    user = user_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return user_id_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
 
 
 def get_secret_key():
--- bookItemsApi/user.py
+++ bookItemsApi/user.py
@@ -1,3 +1,6 @@
+import sqlite3
+from FlaskAPI.bookItemsApi.config.UserDB import UserDBSqlLiteTable
+
 class User:
 
     def __init__(self, _id, username, password):
@@ -5,3 +8,30 @@ class User:
         self.username = username
         self.password = password
 
+    @classmethod
+    def find_by_username(cls, username):
+        connection = UserDBSqlLiteTable()
+
+        result = connection.get_user_by_username(username)
+        if result:
+            user = cls(*result)
+
+        else:
+            user=None
+
+        connection.close_connection()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = UserDBSqlLiteTable()
+
+        result = connection.get_user_by_id(_id)
+        if result:
+            user = cls(*result)
+
+        else:
+            user = None
+
+        connection.close_connection()
+        return user
